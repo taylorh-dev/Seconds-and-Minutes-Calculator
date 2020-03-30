8308c8c93adb1067f90bfa264eb53b7fc8344788
taylorh-dev <61665125+taylorh-dev@users.noreply.github.com>
2020-03-30 18:38:32 -0500

    Add files via upload

--- SecondsAndMinutes.java
+++ SecondsAndMinutes.java
@@ -2,16 +2,18 @@ package com.taylorh.dev;
 
 public class SecondsAndMinutes {
 
-    public static String getDurationString(int minutes, int seconds) {
+    private static final String INVALID_VALUE_MESSAGE = "Invalid Value";
+
+    public static String getDurationString(long minutes, long seconds) {
 
         if (minutes < 0 || seconds < 0 || seconds > 60) {
-            return "Invalid Value";
+            return INVALID_VALUE_MESSAGE;
         }
 
-        int hours = minutes / 60;
-        int remainingMinutes = minutes % 60;
-        int secondsToMinutes = seconds / 60;
-        int remainingSeconds = seconds % 60;
+        long hours = minutes / 60;
+        long remainingMinutes = minutes % 60;
+        long secondsToMinutes = seconds / 60;
+        long remainingSeconds = seconds % 60;
 
         remainingMinutes += secondsToMinutes;
 
@@ -21,11 +23,11 @@ public class SecondsAndMinutes {
     public static String getDurationString(int seconds) {
 
         if (seconds < 0) {
-            return "Invalid Value";
+            return INVALID_VALUE_MESSAGE;
         }
 
-        int minutes = seconds / 60;
-        int secondsRemainder = seconds % 60;
+        long minutes = seconds / 60;
+        long secondsRemainder = seconds % 60;
 
         return getDurationString(minutes, secondsRemainder);
     }
